e565c2d8e314748556e902bf6bfca1c1c32eab85
viraj.k <viraj.k@Viraj-IT.arpiconet.com>
2017-05-22 09:10:22 +0530

    test

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Viraj");
         // TODO code application logic here
     }
     
